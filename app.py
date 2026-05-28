3b45c061b5f912991c9c9082178a85019d1c93b1
isaac1925gato <isaacdelta1985@gmail.com>
2026-05-28 19:01:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,37 +8,64 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 
-@app.route("/")
 
+@app.route("/")
 def home():
 
     return """
     <h1>MercadoLibre Stock</h1>
 
     <a href='/login'>
-    LOGIN MERCADOLIBRE
+        LOGIN MERCADOLIBRE
     </a>
+
     """
 
-@app.route("/login")
 
+@app.route("/login")
 def login():
 
-    url=f"https://auth.mercadolibre.cl/authorization?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
+    url = f"https://auth.mercadolibre.cl/authorization?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
 
     return redirect(url)
 
-@app.route("/callback")
 
+@app.route("/callback")
 def callback():
 
-    code=request.args.get("code")
+    code = request.args.get("code")
 
-    return f"Codigo recibido correctamente:<br><br>{code}"
+    data = {
 
-if __name__=="__main__":
+        "grant_type": "authorization_code",
+
+        "client_id": CLIENT_ID,
+
+        "client_secret": CLIENT_SECRET,
+
+        "code": code,
+
+        "redirect_uri": REDIRECT_URI
+
+    }
+
+    r = requests.post(
+
+        "https://api.mercadolibre.com/oauth/token",
+
+        data=data
+
+    )
+
+    return f"<pre>{r.text}</pre>"
+
+
+if __name__ == "__main__":
 
     app.run(
+
         host="0.0.0.0",
-        port=int(os.environ.get("PORT",5000))
+
+        port=int(os.environ.get("PORT", 5000))
+
     )
