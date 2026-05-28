81130c05ce34a1d5c993ef65c98e5dd816a9a50f
isaac1925gato <isaacdelta1985@gmail.com>
2026-05-28 19:04:21 -0400

    app.py

--- app.py
+++ app.py
@@ -8,24 +8,52 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 
+ACCESS_TOKEN = None
 
 @app.route("/")
 def home():
 
+    global ACCESS_TOKEN
+
+    if ACCESS_TOKEN:
+
+        headers={
+            "Authorization":f"Bearer {ACCESS_TOKEN}"
+        }
+
+        url="https://api.mercadolibre.com/users/me"
+
+        data=requests.get(
+            url,
+            headers=headers
+        ).json()
+
+        return f"""
+        <h1>Conectado</h1>
+
+        Usuario:
+
+        <br><br>
+
+        {data}
+        """
+
     return """
+
     <h1>MercadoLibre Stock</h1>
 
     <a href='/login'>
-        LOGIN MERCADOLIBRE
+
+    LOGIN MERCADOLIBRE
+
     </a>
 
     """
 
-
 @app.route("/login")
 def login():
 
-    url = f"https://auth.mercadolibre.cl/authorization?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
+    url=f"https://auth.mercadolibre.cl/authorization?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}"
 
     return redirect(url)
 
@@ -33,39 +61,43 @@ def login():
 @app.route("/callback")
 def callback():
 
-    code = request.args.get("code")
+    global ACCESS_TOKEN
 
-    data = {
+    code=request.args.get("code")
 
-        "grant_type": "authorization_code",
-
-        "client_id": CLIENT_ID,
-
-        "client_secret": CLIENT_SECRET,
-
-        "code": code,
-
-        "redirect_uri": REDIRECT_URI
-
-    }
-
-    r = requests.post(
+    response=requests.post(
 
         "https://api.mercadolibre.com/oauth/token",
 
-        data=data
+        data={
+
+            "grant_type":"authorization_code",
+
+            "client_id":CLIENT_ID,
+
+            "client_secret":CLIENT_SECRET,
+
+            "code":code,
+
+            "redirect_uri":REDIRECT_URI
+
+        }
 
     )
 
-    return f"<pre>{r.text}</pre>"
+    data=response.json()
+
+    ACCESS_TOKEN=data["access_token"]
+
+    return redirect("/")
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
 
     app.run(
 
         host="0.0.0.0",
 
-        port=int(os.environ.get("PORT", 5000))
+        port=int(os.environ.get("PORT",5000))
 
     )
